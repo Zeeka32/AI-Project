773bbc8efc0140e7ced743c758a230dd0da16574
Zeeka32 <Zeeka32@gmail.com>
2023-05-19 13:10:33 +0300

    somewhat done

--- AI.py
+++ AI.py
@@ -17,8 +17,7 @@ def connect4_ai(board, turn, depth):
     alpha = float('-inf')
     beta = float('inf')
     maxi(copyboard, depth, c, alpha, beta)
-    values = [50, 150, 250, 350, 450, 550, 650]
-    return values[c[0]]
+    return c[0]
 
 
 def mini(board, depth, alpha, beta):
@@ -88,7 +87,7 @@ def maxi(board, depth, c, alpha, beta):
         for i in range(7):
             if empty[i] != -1:
                 board[empty[i]][i] = 1
-                maximmum = score_position(board, 1)
+                maximmum = max(get_score(board, empty[i], i), maximmum)
                 board[empty[i]][i] = 0
 
                 #alpha beta pruning
@@ -117,89 +116,72 @@ def maxi(board, depth, c, alpha, beta):
 
     return maximmum
 
-def score_position(board, piece):
-    score = 0
+def horizon(board, color, i, j, v):
+    if j not in range(0, len(board[0])) or board[i][j] != color:
+        return 0
 
-    # Evaluate winning positions
-    score += 8 * count_windows(board, 4, piece)  # Four in a row
-    score += 4 * count_windows(board, 3, piece)  # Three in a row
+    return 1 + horizon(board, color, i, j + v, v)
 
-    # Evaluate number of possible winning lines
-    score += 3 * count_winning_lines(board, piece)
 
-    # Evaluate center column preference
-    center_column = board[:, 7 // 2]
-    score += 4 * np.count_nonzero(center_column == piece)
+def vertical(board, color, i, j, v):
+    if i not in range(0, len(board)) or board[i][j] != color:
+        return 0
 
-    # Evaluate piece count
-    ai_count = np.count_nonzero(board == piece)
-    player_count = np.count_nonzero(board == (3 - piece))
-    score += 2 * (ai_count - player_count)
+    return 1 + vertical(board, color, i + v, j, v)
+
+
+def diagonal(board, color, i, j, v, e):
+    if j not in range(0, len(board[0])) or i not in range(0, len(board)) or board[i][j] != color:
+        return 0
+
+    return 1 + diagonal(board, color, i + v, j + e, v, e)
+
+
+def is_win(board, i, j):
+    cons = max(horizon(board, board[i][j], i, j, -1) + horizon(board, board[i][j], i, j, 1),
+               vertical(board, board[i][j], i, j, -1) + vertical(board, board[i][j], i, j, 1),
+               diagonal(board, board[i][j], i, j, -1, 1) + diagonal(board, board[i][j], i, j, 1, -1),
+               diagonal(board, board[i][j], i, j, -1, -1) + diagonal(board, board[i][j], i, j, 1, 1)) - 1
+
+    return True if cons == 4 else False
+
+
+def get_score_core(board, i, j):
+    l = [horizon(board, board[i][j], i, j, -1) + horizon(board, board[i][j], i, j, 1) - 1,
+         vertical(board, board[i][j], i, j, -1) + vertical(board, board[i][j], i, j, 1) - 1,
+         diagonal(board, board[i][j], i, j, -1, 1) + diagonal(board, board[i][j], i, j, 1, -1) - 1,
+         diagonal(board, board[i][j], i, j, -1, -1) + diagonal(board, board[i][j], i, j, 1, 1) - 1]
+
+    two = l.count(2)
+    three = l.count(3)
+
+    score = 4 if j == 3 else 0
+    score += two * 2 + three * 5
 
     return score
 
-def count_windows(board, num_pieces, piece):
-    count = 0
 
-    # Check horizontal windows
-    for r in range(6):
-        for c in range(7 - num_pieces + 1):
-            window = board[r, c:c+num_pieces]
-            if np.count_nonzero(window == piece) == num_pieces:
-                count += 1
+def get_score(board, i, j):
+    color = board[i][j]
+    sum = 0
+    for i in range(6):
+        for j in range(7):
+            if board[i][j] == color:
+                sum += get_score_core(board, i, j)
 
-    # Check vertical windows
-    for c in range(7):
-        for r in range(6 - num_pieces + 1):
-            window = board[r:r+num_pieces, c]
-            if np.count_nonzero(window == piece) == num_pieces:
-                count += 1
+    return sum
 
-    # Check positively sloped diagonal windows
-    for r in range(6 - num_pieces + 1):
-        for c in range(7 - num_pieces + 1):
-            window = [board[r+i][c+i] for i in range(num_pieces)]
-            if np.count_nonzero(window == piece) == num_pieces:
-                count += 1
 
-    # Check negatively sloped diagonal windows
-    for r in range(6 - num_pieces + 1):
-        for c in range(7 - num_pieces + 1):
-            window = [board[r+num_pieces-1-i][c+i] for i in range(num_pieces)]
-            if np.count_nonzero(window == piece) == num_pieces:
-                count += 1
+def get_empty(board):
+    empty = [-1] * 7
+    for i in range(7):
+        for j in range(6):
+            if board[j][i] == 0:
+                empty[i] = j
+                break
 
-    return count
+    return empty
 
-def count_winning_lines(board, piece):
-    count = 0
 
-    # Check horizontal winning lines
-    for r in range(6):
-        for c in range(4):
-            window = board[r, c:c+4]
-            if np.count_nonzero(window == piece) == 3 and np.count_nonzero(window == 0) == 1:
-                count += 1
-
-    # Check vertical winning lines
-    for c in range(7):
-        for r in range(3):
-            window = board[r:r+4, c]
-            if np.count_nonzero(window == piece) == 3 and np.count_nonzero(window == 0) == 1:
-                count += 1
-
-    # Check positively sloped diagonal winning lines
-    for r in range(3):
-        for c in range(4):
-            window = [board[r+i][c+i] for i in range(4)]
-            if np.count_nonzero(window == piece) == 3 and np.count_nonzero(window == 0) == 1:
-                count += 1
-
-    # Check negatively sloped diagonal winning lines
-    for r in range(3):
-        for c in range(3):
-            window = [board[r+3-i][c+i] for i in range(4)]
-            if np.count_nonzero(window == piece) == 3 and np.count_nonzero(window == 0) == 1:
-                count += 1
-
-    return count
+def is_draw(board):
+    return False if any(0 in row for row in board) else True
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from board import Board
 import time
-import random
+from AI import *
 
 # GAME LINK
 # http://kevinshannon.com/connect4/
@@ -18,14 +18,15 @@ def main():
         board.print_grid(game_board)
         print("\n")
 
+        game_board = np.array(game_board)
         # YOUR CODE GOES HERE
 
         # Insert here the action you want to perform based on the output of the algorithm
         # You can use the following function to select a column
-        random_column = random.randint(0, 6)
-        board.select_column(6)
+        best_col = connect4_ai(game_board, 1, 4)
+        board.select_column(best_col)
 
-        time.sleep(2)
+        time.sleep(3)
 
 
 if __name__ == "__main__":
